b61a29220b4125111264de74a3d6fccd7362f35d
Kamran Shabbir <kamranshabbirofficial@gmail.com>
2025-01-03 20:35:47 +0500

    Firebase Web App Live

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -85,7 +85,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "5bc77b131d5a06bd8d9ba4af1e2c308e
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "3afbac9e162120439f326768aa05b0d2",
+"flutter_bootstrap.js": "b2ea7c22702e28f2fae4bba92f628468",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
